603e5aa872fca351a5e3bb1570496d2d81db80bc
Hichami Houssam <hichami.houssam@gmail.com>
2021-09-28 15:36:54 +0100

    task1: testing throwing errors func

--- stringlength.js
+++ stringlength.js
@@ -1,6 +1,27 @@
 function stringLength(string) {
 
+    if(string.length === 0){
+
+        throw new Error('String empty');
+        
+        
+    }
+
+    else if(string.length >= 10){
+
+        throw new Error('String too long!');
+    
+    }
+
     return string.length;
+
+
+
+    
+
+    
+
+    
     
 }
 
--- stringlength.test.js
+++ stringlength.test.js
@@ -1,8 +1,18 @@
 const { expect } = require('@jest/globals')
 const stringLength = require('./stringlength')
 
-test(`the length is right (= ${stringLength('relentlessly')}) `, () => {
-    expect(stringLength('relentlessly')).toBe(12)
+test("the length is right", () => {
+    expect(stringLength('hello man')).toBe(9)
 
 });
+
+test("should throw an error if string too long", ()=> {
+    expect(() => {stringLength('this is great to see !');}).toThrow('String too long!');
+});
+
+test("should throw an error if string empty", ()=> {
+    expect(() => {stringLength('');}).toThrow('String empty');
+});
+
+
 
